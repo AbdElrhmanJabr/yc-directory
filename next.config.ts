220489317bc28ec78ee5c81f1de0d14fa5423b80
Abd Elrhman Jabr <abdelrhamnjabr@gmail.com>
2025-03-06 14:22:53 +0200

    Try ro solve deployment issue

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds : true,
   },
   experimental: {
-    ppr : 'incremental'
+    ppr : 'incremental',
   },
   devIndicators :{
     appIsrStatus : true , 
