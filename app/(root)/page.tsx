c2b4d024a180e9c9b0e5d9cb33aa953b0c0c7676
Abd Elrhman Jabr <abdelrhamnjabr@gmail.com>
2025-03-06 15:10:10 +0200

    couldnt deployon vercel  because the ppr is still on the canarry version not on the stable version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
 
+
 export default async function Home({
   searchParams,
 }: {
